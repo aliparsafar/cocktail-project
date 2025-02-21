fe4a21f2a0e724990bb0144e9c09ac98711d7e7b
aliparsafar <aliparsafar.code@gmail.com>
2025-02-21 12:29:01 +0330

    edit Detailcontent (project completed)

--- src/components/DetailContent.jsx
+++ src/components/DetailContent.jsx
@@ -3,7 +3,7 @@ export default function DetailContent({idDrink, strDrinkThumb, strDrink, strCate
         <div className="flex flex-row mt-10  shadow-2xl rounded-4xl">
            
             <img src={strDrinkThumb} alt="" className="w-3xl rounded-4xl"/>
-            <div className="flex flex-col justify-center gap-5 ml-10 text-lg  ">
+            <div className="flex flex-col justify-center gap-5 ml-10 text-2xl text-emerald-700  ">
             <span>Name : {strDrink}</span>
             <span>Category : {strCategory}</span>
             <span>Info : {strAlcoholic}</span>
